0c3c06ec7f5b3ec86e1e0926ea6faf7558aa191c
Otávio Cardoso <81239817+ma1on3se@users.noreply.github.com>
2021-11-23 22:02:46 -0300

    chore(*): dice test

---
+++ assets/js/index.js
@@ -0,0 +1,19 @@
+/* var dado;
+
+function iniciar(){
+    var button = document.getElementById("rolar");
+    button.addEventListener("click", rolar, false);
+    dado = document.getElementById("dado");
+}
+
+function rolar(){
+    setImage( dado );
+}
+
+function setImage(dado){
+    var valor = Math.floor(1 + Math.random() * 6)
+    dado.setAttribute("./assets/img/", "dice-" + valor + ".png");
+}
+
+window.addEventListener("load", iniciar, false); */
+
